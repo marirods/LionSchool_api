982396ca6cbce8041287d1884b4bed8dd4fd1dac
MARIANE DA SILVA RODRIGUES <24122311@dsn127m1249389.cfp127aluno.local>
2025-04-29 10:09:50 -0300

    tela tres finalizada

---
+++ app/src/main/java/br/senai/sp/jandira/lionschool/components/Alunos.kt
@@ -0,0 +1,165 @@
+package br.senai.sp.jandira.lionschool.components
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
+
+
+@Composable
+
+
+fun Alunos(
+    image: Painter,
+    name: String,
+    statusColor: Color,
+    data: String,
+    id: String,
+    onClick:()->Unit
+) {
+    val cardShape = RoundedCornerShape(16.dp)
+
+    // Card com bordas arredondadas e sombra
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .height(90.dp),
+        shape = cardShape,
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White) // Cor do card
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(0xff9FA9E1),
+                            Color(0xff9FA9E1)
+                        )
+                    ),
+                    shape = cardShape
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                // Dados do estudante (imagem, nome e id)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Indicador de status
+                    Card(
+                        modifier = Modifier
+                            .height(90.dp)
+                            .width(20.dp),
+                        colors = CardDefaults.cardColors(containerColor = statusColor),
+                        shape = RoundedCornerShape(
+                            topEnd = 0.dp,
+                            bottomEnd = 0.dp,
+                            topStart = 16.dp,
+                            bottomStart = 16.dp
+                        )
+                    ) {}
+
+                    // Imagem do estudante
+                    Image(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .size(60.dp),
+                        painter = image,
+                        contentDescription = "perfil"
+                    )
+
+                    // Nome e ID do estudante
+                    Column {
+                        Text(
+                            modifier = Modifier,
+                            text = name,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(20.dp),
+                                imageVector = Icons.Default.DateRange,
+                                contentDescription = "id",
+                                tint = Color.White
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .padding(start = 5.dp),
+                                text = id,
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+
+                // Data do estudante
+                Row(
+                    modifier = Modifier
+                        .padding(7.dp)
+                        .height(90.dp),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(20.dp),
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "data",
+                        tint = Color.White
+                    )
+                    Text(
+                        text = data,
+                        color = Color(0xFFFFC23D),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun AlunosPreview() {
+    Alunos(
+        image = painterResource(R.drawable.perfil_img1),
+        name = "Luana Oliveira Dias",
+        statusColor = Color.Yellow,
+        data = "2023",
+        id = "RM12345",
+        onClick = {}
+    )
+}
--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaAlunos.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/TelaAlunos.kt
@@ -17,8 +17,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -29,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -39,121 +43,190 @@ import br.senai.sp.jandira.lionschool.R
 import java.util.Locale
 
 @Composable
-
 fun TelaAlunos(modifier: Modifier = Modifier) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    listOf(
-                        Color(color = 0xFFFFFFFF),
-                        Color(color = 0xFFFFFFFF)
 
-                    )
-                )
-            ),
     ) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxSize()
-                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .padding(20.dp)
         ) {
-            Row(
-                modifier = Modifier
-                    .padding(top = 26.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-
+            Column(
             ) {
                 Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.lion_logo),
-                        contentDescription = stringResource(R.string.logo_description),
+                    Row(
                         modifier = Modifier
-                            .size(50.dp)
-                    )
+                            .background(Color.White),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .padding(4.dp),
+                            painter = painterResource(R.drawable.lion_logo),
+                            contentDescription = stringResource(R.string.logo_description)
+                        )
+                        Text(
+                            text = stringResource(R.string.lion_nome),
+                            color = Color(0xFF3347B0),
+                            fontWeight = FontWeight.Bold
+                        )
+
+                    }
+                    Card(
+                        modifier = Modifier,
+                        colors = CardDefaults.cardColors(containerColor = Color(0xFFFFC23D)),
+                        shape = CircleShape
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(10.dp),
+                            text = stringResource(R.string.ds1),
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
-
-                Spacer(
-                    modifier = Modifier
-                        .width(8.dp)
+                Divider(
+                    color = Color(0xFFFFC23D)
                 )
-
                 Column(
                     modifier = Modifier
-                        .weight(1f),
-                    horizontalAlignment = AbsoluteAlignment.Right
-
+                        .fillMaxWidth()
+                        .padding(top = 5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text(
-                        text = stringResource(R.string.lion_nome),
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color(color = 0xFF3347B0)
-
+                    OutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        value = "",
+                        onValueChange = {
+                        },
+                        label = { Text(text = stringResource(R.string.pesquisar)) },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = "",
+                                tint = (Color(color = 0xFF9E9FA4))
+                            )
+                        }
                     )
-
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .padding(5.dp),
+                            colors = ButtonDefaults.buttonColors(Color(0xFF3347B0))
+                        ) {
+                            Text(
+                                text = stringResource(R.string.todos_curso)
+                            )
+                        }
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .padding(5.dp),
+                            colors = ButtonDefaults.buttonColors(Color(0xFFFFC23D))
+                        ) {
+                            Text(
+                                text = stringResource(R.string.cursando),
+                                color = Color(0xFF3347B0)
+                            )
+                        }
+                        Button(
+                            onClick = {},
+                            modifier = Modifier
+                                .padding(5.dp),
+                            colors = ButtonDefaults.buttonColors(Color(0xFFFFC23D))
+                        ) {
+                            Text(
+                                text = stringResource(R.string.finalizado),
+                                color = Color(0xFF3347B0)
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.lion_list),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(
+                                    (100.dp)
+                                )
+                        )
+                        Text(
+                            text = stringResource(R.string.list_student),
+                            color = Color(0xFF3347B0),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 35.sp
+                        )
+                    }
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img1),
+                        name = stringResource(R.string.perfil_name1),
+                        statusColor = colorResource(R.color.cursando),
+                        data = stringResource(R.string.data1),
+                        id = stringResource(R.string.id1)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img2),
+                        name = stringResource(R.string.perfil_name2),
+                        statusColor = colorResource(R.color.cursando),
+                        data = stringResource(R.string.data2),
+                        id = stringResource(R.string.id2)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img3),
+                        name = stringResource(R.string.perfil_name3),
+                        statusColor = colorResource(R.color.finalizado),
+                        data = stringResource(R.string.data3),
+                        id = stringResource(R.string.id3)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img4),
+                        name = stringResource(R.string.perfil_name4),
+                        statusColor = colorResource(R.color.finalizado),
+                        data = stringResource(R.string.data4),
+                        id = stringResource(R.string.id4)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img5),
+                        name = stringResource(R.string.perfil_name5),
+                        statusColor = colorResource(R.color.finalizado),
+                        data = stringResource(R.string.data5),
+                        id = stringResource(R.string.id5)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img67),
+                        name = stringResource(R.string.perfil_name6),
+                        statusColor = colorResource(R.color.finalizado),
+                        data = stringResource(R.string.data6),
+                        id = stringResource(R.string.id6)
+                    )
+                    Alunos(
+                        image = painterResource(R.drawable.perfil_img67),
+                        name = stringResource(R.string.perfil_name7),
+                        statusColor = colorResource(R.color.finalizado),
+                        data = stringResource(R.string.data7),
+                        id = stringResource(R.string.id7)
+                    )
                 }
-
             }
-
-            Card (
-                modifier = Modifier
-                    .size(100.dp),
-                shape = CircleShape,
-                border = BorderStroke(
-                    width = 0.dp,
-                    color = Color(0xFFFFFFFF)
-                )
-            ) {
-            }
-        }
-
-
-        Column(
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                text = stringResource(R.string.lion_nome),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color(color = 0xFF3347B0)
-
-            )
         }
     }
-    HorizontalDivider(
-        modifier = Modifier
-            .padding(
-                top = 10.dp,
-                start = 20.dp,
-                end = 20.dp
-
-            ),
-        color = Color(color = 0xFFFFC23D)
-    )
-    OutlinedTextField(
-        value = "",
-        onValueChange = {},
-        label = {
-            Text(stringResource(R.string.find_student))
-        },
-        trailingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = Color.Black
-            )
-        },
-        modifier = Modifier
-            .padding(15.dp)
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(10.dp)
-    )
 }
 
 
